a40bb63662556e4c18908159571300707142f1ae
liujinye <liujinye@xiaomi.com>
2018-04-14 17:06:24 +0800

    Merge "Update README.md" am: 7188198fd8 am: e8cbba02c6 am: ba6b158f43
    Change-Id: I71da9bf5a80b94fb36b451b53ee58038b9c9dc28

--- build/properties.go
+++ build/properties.go
@@ -47,7 +47,6 @@ type ccProperties struct {
 	Export_shared_lib_headers []string
 	Export_static_lib_headers []string
 	Export_generated_headers  []string
-	Double_loadable           *bool
 }
 
 type javaProperties struct {
