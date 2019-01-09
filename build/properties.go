0db3b4ba1791a385fb7b9a57a2e4e307fe35bf46
openvela-robot <openvela-robot@xiaomi.com>
2019-01-09 18:00:46 +0800

    Revert "Revert "Fillout requesting SID.""
    This reverts commit 84bde5aaacfec0906e43d05a4517f2739e53d0f1.
    
    This enables getting security contexts from the kernel.
    
    Bug: 121035042
    Test: boot patch w/ w/o kernel patch on cuttlefish and check for
        security logs.
    Test: check for cnd failures on crosshatch.
    
    Change-Id: Ie33e7df88859f29b8197faf89e801c74a75dba00
    Merged-In: Ie33e7df88859f29b8197faf89e801c74a75dba00
    (cherry picked from commit 1209ed7c1b634c892b0de5f2443d0630f4d40dda)

--- build/properties.go
+++ build/properties.go
@@ -24,21 +24,12 @@ type fileGroupProperties struct {
 	Srcs  []string
 }
 
-type genruleProperties struct {
-	Name    *string
-	Owner   *string
-	Tools   []string
-	Cmd     *string
-	Srcs    []string
-	Out     []string
-	Depfile *bool
-}
-
 type ccProperties struct {
 	Name                      *string
 	Owner                     *string
 	Defaults                  []string
 	Vendor_available          *bool
+	Recovery_available        *bool
 	Generated_sources         []string
 	Generated_headers         []string
 	Group_static_libs         *bool
@@ -47,6 +38,7 @@ type ccProperties struct {
 	Export_shared_lib_headers []string
 	Export_static_lib_headers []string
 	Export_generated_headers  []string
+	Double_loadable           *bool
 }
 
 type javaProperties struct {
@@ -54,6 +46,7 @@ type javaProperties struct {
 	Owner             *string
 	Defaults          []string
 	No_framework_libs *bool
+	Installable       *bool
 	Sdk_version       *string
 	Srcs              []string
 	Libs              []string
