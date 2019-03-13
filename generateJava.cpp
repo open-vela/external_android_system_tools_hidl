f278981dc3016aee0baf00708501b429ae8182f5
liujinye <liujinye@xiaomi.com>
2019-03-13 23:27:56 +0800

    Merge "Add '-java-shallow' library."

--- generateJava.cpp
+++ generateJava.cpp
@@ -71,19 +71,6 @@ void emitGetService(
         const std::string& ifaceName,
         const std::string& fqName,
         bool isRetry) {
-    if (isRetry) {
-        DocComment(
-                "This will invoke the equivalent of the C++ getService(std::string) if retry is\n"
-                "true or tryGetService(std::string) if retry is false. If the service is\n"
-                "available on the device and retry is true, this will wait for the service to\n"
-                "start. Otherwise, it will return immediately even if the service is null.")
-                .emit(out);
-    } else {
-        DocComment(
-                "Warning: this will not wait for the interface to come up if it hasn't yet\n"
-                "started. See getService(String,boolean) instead.")
-                .emit(out);
-    }
     out << "public static "
         << ifaceName
         << " getService(String serviceName";
@@ -103,14 +90,6 @@ void emitGetService(
         out << "));\n";
     }).endl().endl();
 
-    if (isRetry) {
-        DocComment("Calls getService(\"default\",retry).").emit(out);
-    } else {
-        DocComment(
-                "Warning: this will not wait for the interface to come up if it hasn't yet "
-                "started. See getService(String,boolean) instead.")
-                .emit(out);
-    }
     out << "public static "
         << ifaceName
         << " getService(";
@@ -149,8 +128,6 @@ void AST::generateJava(Formatter& out, const std::string& limitToType) const {
 
     const Interface *superType = iface->superType();
 
-    iface->emitDocComment(out);
-
     out << "public interface " << ifaceName << " extends ";
 
     if (superType != nullptr) {
@@ -162,14 +139,12 @@ void AST::generateJava(Formatter& out, const std::string& limitToType) const {
     out << " {\n";
     out.indent();
 
-    DocComment("Fully-qualified interface name for this interface.").emit(out);
     out << "public static final String kInterfaceName = \""
         << mPackage.string()
         << "::"
         << ifaceName
         << "\";\n\n";
 
-    DocComment("Does a checked conversion from a binder to this class.").emit(out);
     out << "/* package private */ static "
         << ifaceName
         << " asInterface(android.os.IHwBinder binder) {\n";
@@ -221,7 +196,6 @@ void AST::generateJava(Formatter& out, const std::string& limitToType) const {
     out.unindent();
     out << "}\n\n";
 
-    DocComment("Does a checked conversion from any interface to this class.").emit(out);
     out << "public static "
         << ifaceName
         << " castFrom(android.os.IHwInterface iface) {\n";
@@ -239,7 +213,7 @@ void AST::generateJava(Formatter& out, const std::string& limitToType) const {
     emitGetService(out, ifaceName, iface->fqName().string(), true /* isRetry */);
     emitGetService(out, ifaceName, iface->fqName().string(), false /* isRetry */);
 
-    iface->emitJavaTypeDeclarations(out, false /* atTopLevel */);
+    emitJavaTypeDeclarations(out);
 
     for (const auto &method : iface->methods()) {
         const bool returnsValue = !method->results().empty();
@@ -670,6 +644,10 @@ void AST::generateJava(Formatter& out, const std::string& limitToType) const {
 
     out.unindent();
     out << "}\n";
+}
+
+void AST::emitJavaTypeDeclarations(Formatter& out) const {
+    mRootScope.emitJavaTypeDeclarations(out, false /* atTopLevel */);
 }
 
 }  // namespace android
