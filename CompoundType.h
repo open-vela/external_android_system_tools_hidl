26dc8e72ee5b9a5414f6b502e3314c5c4f4a59c6
liujinye <liujinye@xiaomi.com>
2019-05-02 20:57:55 +0800

    Merge "Additional tests for malformed packets." into qt-dev

--- CompoundType.h
+++ CompoundType.h
@@ -188,6 +188,13 @@ private:
                                               bool isCopyConstructor,
                                               bool usesMoveSemantics) const;
 
+    struct SafeUnionEnumElement {
+        std::string fieldName;
+        std::string fieldTypeName;
+    };
+
+    std::vector<SafeUnionEnumElement> getSafeUnionEnumElements(bool useCppTypeName) const;
+
     CompoundLayout getCompoundAlignmentAndSize() const;
     void emitPaddingZero(Formatter& out, size_t offset, size_t size) const;
 
