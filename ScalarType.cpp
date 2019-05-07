78f77619dc30cf5f8dd04336275cac8020a5f823
liujinye <liujinye@xiaomi.com>
2019-05-07 22:00:44 +0800

    Snap for 5450365 from fab4a3fa419158001944ec0a439fd4471881aa77 to pi-platform-release
    Change-Id: I644e451bbfa0376dfc6bdc7f5c99c0ceaf3792ea

--- ScalarType.cpp
+++ ScalarType.cpp
@@ -83,7 +83,7 @@ std::string ScalarType::getJavaType(bool /* forInitializer */) const {
     return kName[mKind];
 }
 
-std::string ScalarType::getJavaTypeClass() const {
+std::string ScalarType::getJavaWrapperType() const {
     static const char *const kName[] = {
         "Boolean",
         "Byte",
@@ -225,7 +225,7 @@ void ScalarType::emitConvertToJavaHexString(
         case KIND_INT16:    // fallthrough
         case KIND_UINT16: {
             // Because Byte and Short doesn't have toHexString, we have to use Integer.toHexString.
-            out << "Integer.toHexString(" << getJavaTypeClass() << ".toUnsignedInt(("
+            out << "Integer.toHexString(" << getJavaWrapperType() << ".toUnsignedInt(("
                 << getJavaType(false /* forInitializer */) << ")(" << name << ")))";
             break;
         }
@@ -233,7 +233,7 @@ void ScalarType::emitConvertToJavaHexString(
         case KIND_UINT32:   // fallthrough
         case KIND_INT64:    // fallthrough
         case KIND_UINT64: {
-            out << getJavaTypeClass() << ".toHexString(" << name << ")";
+            out << getJavaWrapperType() << ".toHexString(" << name << ")";
             break;
         }
         case KIND_FLOAT:    // fallthrough
--- utils/Formatter.cpp
+++ utils/Formatter.cpp
@@ -22,10 +22,10 @@
 
 namespace android {
 
-Formatter::Formatter() : mFile(nullptr /* invalid */), mIndentDepth(0), mAtStartOfLine(true) {}
+Formatter::Formatter() : mFile(NULL /* invalid */), mIndentDepth(0), mAtStartOfLine(true) {}
 
 Formatter::Formatter(FILE* file, size_t spacesPerIndent)
-    : mFile(file == nullptr ? stdout : file),
+    : mFile(file == NULL ? stdout : file),
       mIndentDepth(0),
       mSpacesPerIndent(spacesPerIndent),
       mAtStartOfLine(true) {}
@@ -34,7 +34,7 @@ Formatter::~Formatter() {
     if (mFile != stdout) {
         fclose(mFile);
     }
-    mFile = nullptr;
+    mFile = NULL;
 }
 
 void Formatter::indent(size_t level) {
