67eda62f750f044b4b7a74d123277f2d34667577
liujinye <liujinye@xiaomi.com>
2019-05-09 19:54:47 +0800

    Add missing newline. am: 3768f42dff
    Change-Id: I52dbf0eae41644fa7244d5ea51063b8bd2123da7

--- generateJava.cpp
+++ generateJava.cpp
@@ -139,6 +139,10 @@ void AST::generateJava(Formatter& out, const std::string& limitToType) const {
     const std::string ifaceName = iface->localName();
     const std::string baseName = iface->getBaseName();
 
+    std::vector<std::string> packageComponents;
+    getPackageAndVersionComponents(
+            &packageComponents, true /* cpp_compatible */);
+
     out << "package " << mPackage.javaPackage() << ";\n\n";
 
     out.setNamespace(mPackage.javaPackage() + ".");
