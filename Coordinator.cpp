cbd5393f77c5e5bacd9fcb4358f0b3bbc91cfbb9
liujinye <liujinye@xiaomi.com>
2019-05-22 19:17:23 +0800

    Remove Interface::hasOnewayMethods() am: 19f7979db9 am: a4f8f3eac3 am: ff97ac22f0 am: c9c75638cf
    Change-Id: I9ab864124630f35b848fd5311f0bee8740991e0d

--- Coordinator.cpp
+++ Coordinator.cpp
@@ -946,7 +946,7 @@ bool Coordinator::MakeParentHierarchy(const std::string &path) {
 void Coordinator::parseOptions(int argc, char** argv, const std::string& options,
                                const HandleArg& handleArg) {
     // reset global state for getopt
-    optind = 1;
+    optind = 0;
 
     bool suppressDefaultPackagePaths = false;
 
