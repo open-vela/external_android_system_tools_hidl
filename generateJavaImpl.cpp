d43422068c3e5ac005de9eaf8b9692f7f4eb8a0f
openvela-robot <openvela-robot@xiaomi.com>
2019-05-24 19:25:44 +0800

    Export headers from libhidl-gen-hash am: ddae1b6d93
    Change-Id: I7cbb171e0abf1a9b34063af13ad08f7277d4390c

--- generateJavaImpl.cpp
+++ generateJavaImpl.cpp
@@ -53,10 +53,28 @@ void AST::generateJavaImpl(Formatter& out) const {
                 prevInterface = superInterface;
             }
 
-            out << "@Override\npublic ";
-            method->emitJavaSignature(out);
+            const bool returnsValue = !method->results().empty();
+            const bool needsCallback = method->results().size() > 1;
 
-            out << "\n";
+            out << "@Override\npublic ";
+            if (returnsValue && !needsCallback) {
+                out << method->results()[0]->type().getJavaType();
+            } else {
+                out << "void";
+            }
+
+            out << " " << method->name() << "(";
+            method->emitJavaArgSignature(out);
+
+            if (needsCallback) {
+                if (!method->args().empty()) {
+                    out << ", ";
+                }
+
+                out << method->name() << "Callback _hidl_cb";
+            }
+
+            out << ")\n";
             out.indent([&] {
                 out.indent();
                 out << "throws android.os.RemoteException {\n";
@@ -65,13 +83,11 @@ void AST::generateJavaImpl(Formatter& out) const {
                 out << "// TODO: Implement\n";
 
                 // Return the appropriate value
-                const bool returnsValue = !method->results().empty();
                 if (returnsValue) {
                     for (const auto& arg : method->results()) {
                         arg->type().emitJavaFieldInitializer(out, arg->name());
                     }
 
-                    const bool needsCallback = method->results().size() > 1;
                     if (needsCallback) {
                         out << "_hidl_cb.onValues(";
 
