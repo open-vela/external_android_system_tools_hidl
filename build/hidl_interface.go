0776263afd2ea4145fb4f478e7bfd513bb57f9db
liujinye <liujinye@xiaomi.com>
2019-06-13 20:43:57 +0800

    Store paths relative to root in Location am: f6293d343b
    Change-Id: Icdc5ac3311d85425b8e62f23920f6b7433156121

--- build/hidl_interface.go
+++ build/hidl_interface.go
@@ -509,9 +509,10 @@ This corresponds to the "-r%s:<some path>" option that would be passed into hidl
 		}, &i.inheritCommonProperties)
 
 		commonJavaProperties := javaProperties{
-			Defaults:    []string{"hidl-java-module-defaults"},
-			Installable: proptools.BoolPtr(true),
-			Srcs:        []string{":" + name.javaSourcesName()},
+			Defaults:          []string{"hidl-java-module-defaults"},
+			No_framework_libs: proptools.BoolPtr(true),
+			Installable:       proptools.BoolPtr(true),
+			Srcs:              []string{":" + name.javaSourcesName()},
 
 			// This should ideally be system_current, but android.hidl.base-V1.0-java is used
 			// to build framework, which is used to build system_current.  Use core_current
@@ -543,10 +544,10 @@ This corresponds to the "-r%s:<some path>" option that would be passed into hidl
 			Outputs:    []string{name.sanitizedDir() + "Constants.java"},
 		}, &i.inheritCommonProperties)
 		mctx.CreateModule(android.ModuleFactoryAdaptor(java.LibraryFactory), &javaProperties{
-			Name:        proptools.StringPtr(name.javaConstantsName()),
-			Defaults:    []string{"hidl-java-module-defaults"},
-			Sdk_version: proptools.StringPtr("core_platform"),
-			Srcs:        []string{":" + name.javaConstantsSourcesName()},
+			Name:              proptools.StringPtr(name.javaConstantsName()),
+			Defaults:          []string{"hidl-java-module-defaults"},
+			No_framework_libs: proptools.BoolPtr(true),
+			Srcs:              []string{":" + name.javaConstantsSourcesName()},
 		}, &i.inheritCommonProperties)
 	}
 
@@ -740,13 +741,11 @@ func hidlInterfaceFactory() android.Module {
 }
 
 var doubleLoadablePackageNames = []string{
-	"android.frameworks.bufferhub@1.0",
 	"android.hardware.cas@1.0",
 	"android.hardware.cas.native@1.0",
 	"android.hardware.configstore@",
 	"android.hardware.drm@1.0",
 	"android.hardware.drm@1.1",
-	"android.hardware.drm@1.2",
 	"android.hardware.graphics.allocator@",
 	"android.hardware.graphics.bufferqueue@",
 	"android.hardware.media@",
