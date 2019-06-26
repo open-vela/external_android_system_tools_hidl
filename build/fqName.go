28bdc74a90c90822959a557c01a9898859313fc0
openvela-robot <openvela-robot@xiaomi.com>
2019-06-26 11:18:14 +0800

    Merge "Stop parsing DocComments attached to typed vars" am: d590166325
    Change-Id: I705f588c44f88c60471d18a5ed84c21443392e7e

--- build/fqName.go
+++ build/fqName.go
@@ -149,7 +149,3 @@ func (f *fqName) vtsProfilerHeadersName() string {
 func (f *fqName) vtsProfilerName() string {
 	return f.string() + "-vts.profiler"
 }
-
-func (f *fqName) lintName() string {
-	return f.string() + "-hidl-lint"
-}
