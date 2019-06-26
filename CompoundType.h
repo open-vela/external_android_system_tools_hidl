e4b822a48a45405846f5c5525593ceda833d3f43
openvela-robot <openvela-robot@xiaomi.com>
2019-06-26 20:35:47 +0800

    Merge "Add a lint for unhandled comments"

--- CompoundType.h
+++ CompoundType.h
@@ -134,12 +134,8 @@ private:
     };
 
     struct CompoundLayout {
-        // Layout of this entire object including metadata.
-        // For struct/union, this is the same as innerStruct.
         Layout overall;
-        // Layout of user-specified data
         Layout innerStruct;
-        // Layout of discriminator for safe union (otherwise zero)
         Layout discriminator;
     };
 
@@ -166,7 +162,6 @@ private:
                                               bool usesMoveSemantics) const;
 
     CompoundLayout getCompoundAlignmentAndSize() const;
-    void emitPaddingZero(Formatter& out, size_t offset, size_t size) const;
 
     void emitSafeUnionReaderWriterForInterfaces(
             Formatter &out,
