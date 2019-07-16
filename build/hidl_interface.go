d9ae22c18d672ad9ade0e86e82da50c7f1eb782a
liujinye <liujinye@xiaomi.com>
2019-07-16 23:52:21 +0800

    Merge "hidl-lint: Non 0 error code when there are lints"

--- build/hidl_interface.go
+++ build/hidl_interface.go
@@ -65,7 +65,7 @@ var (
 	}, "mode", "type", "inputDir", "genDir", "packagePath")
 
 	lintRule = pctx.StaticRule("lintRule", blueprint.RuleParams{
-		Command:     "rm -f ${output} && touch ${output} && ${lint} -j -R -p . ${roots} ${fqName} 2> ${output}",
+		Command:     "rm -f ${output} && touch ${output} && ${lint} -j -e -R -p . ${roots} ${fqName} 2> ${output}",
 		CommandDeps: []string{"${lint}"},
 		Description: "hidl-lint ${fqName}: ${out}",
 	}, "output", "roots", "fqName")
@@ -639,12 +639,16 @@ This corresponds to the "-r%s:<some path>" option that would be passed into hidl
 			Generated_headers:  []string{name.headersName()},
 			Shared_libs: concat(cppDependencies, []string{
 				"libhidlbase",
+				"libhidltransport",
+				"libhwbinder",
 				"liblog",
 				"libutils",
 				"libcutils",
 			}),
 			Export_shared_lib_headers: concat(cppDependencies, []string{
 				"libhidlbase",
+				"libhidltransport",
+				"libhwbinder",
 				"libutils",
 			}),
 			Export_generated_headers: []string{name.headersName()},
@@ -736,6 +740,8 @@ This corresponds to the "-r%s:<some path>" option that would be passed into hidl
 			"libbase",
 			"libcutils",
 			"libhidlbase",
+			"libhidltransport",
+			"libhwbinder",
 			"liblog",
 			"libutils",
 		},
@@ -744,6 +750,7 @@ This corresponds to the "-r%s:<some path>" option that would be passed into hidl
 		}, wrap("", dependencies, "-adapter-helper"), cppDependencies, libraryIfExists),
 		Export_shared_lib_headers: []string{
 			"libhidlbase",
+			"libhidltransport",
 		},
 		Export_static_lib_headers: concat([]string{
 			"libhidladapter",
@@ -768,6 +775,8 @@ This corresponds to the "-r%s:<some path>" option that would be passed into hidl
 			"libbase",
 			"libcutils",
 			"libhidlbase",
+			"libhidltransport",
+			"libhwbinder",
 			"liblog",
 			"libutils",
 		},
@@ -912,13 +921,11 @@ func hidlInterfaceFactory() android.Module {
 }
 
 var doubleLoadablePackageNames = []string{
-	"android.frameworks.bufferhub@1.0",
 	"android.hardware.cas@1.0",
 	"android.hardware.cas.native@1.0",
 	"android.hardware.configstore@",
 	"android.hardware.drm@1.0",
 	"android.hardware.drm@1.1",
-	"android.hardware.drm@1.2",
 	"android.hardware.graphics.allocator@",
 	"android.hardware.graphics.bufferqueue@",
 	"android.hardware.media@",
@@ -939,7 +946,7 @@ func isDoubleLoadable(name string) bool {
 	return false
 }
 
-// packages in libhidlbase
+// packages in libhidltransport
 var coreDependencyPackageNames = []string{
 	"android.hidl.base@",
 	"android.hidl.manager@",
--- main.cpp
+++ main.cpp
@@ -454,7 +454,7 @@ bool isHidlTransportPackage(const FQName& fqName) {
 
 bool isSystemProcessSupportedPackage(const FQName& fqName) {
     // Technically, so is hidl IBase + IServiceManager, but
-    // these are part of libhidlbase.
+    // these are part of libhidltransport.
     return fqName.inPackage("android.hardware.graphics.common") ||
            fqName.inPackage("android.hardware.graphics.mapper") ||
            fqName.string() == "android.hardware.renderscript@1.0" ||
@@ -698,6 +698,7 @@ static status_t generateAndroidBpImplForPackage(Formatter& out, const FQName& pa
             << "shared_libs: [\n";
         out.indent([&] {
             out << "\"libhidlbase\",\n"
+                << "\"libhidltransport\",\n"
                 << "\"libutils\",\n"
                 << "\"" << makeLibraryName(packageFQName) << "\",\n";
 
