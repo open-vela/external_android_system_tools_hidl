2d906b431511e23cdccb0db33a0ecaea018cb0ab
liujinye <liujinye@xiaomi.com>
2019-07-22 23:37:01 +0800

    Merge "Adds FqInstance::getFqName()."

--- CompoundType.h
+++ CompoundType.h
@@ -135,12 +135,8 @@ private:
     };
 
     struct CompoundLayout {
-        // Layout of this entire object including metadata.
-        // For struct/union, this is the same as innerStruct.
         Layout overall;
-        // Layout of user-specified data
         Layout innerStruct;
-        // Layout of discriminator for safe union (otherwise zero)
         Layout discriminator;
     };
 
@@ -167,7 +163,6 @@ private:
                                               bool usesMoveSemantics) const;
 
     CompoundLayout getCompoundAlignmentAndSize() const;
-    void emitPaddingZero(Formatter& out, size_t offset, size_t size) const;
 
     void emitSafeUnionReaderWriterForInterfaces(
             Formatter &out,
