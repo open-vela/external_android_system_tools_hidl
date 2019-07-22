1aee420ad550df2ee4aa468cd028c13155676185
openvela-robot <openvela-robot@xiaomi.com>
2019-07-22 16:40:09 +0800

    Merge "Make hidl_enum_values an inline variable." am: 9fa24cbed3
    Change-Id: Icf2adc3d269a988d39d83bf721f9be4831d01839

--- utils/FqInstance.cpp
+++ utils/FqInstance.cpp
@@ -62,10 +62,6 @@ bool FqInstance::hasInstance() const {
     return !mInstance.empty();
 }
 
-const FQName& FqInstance::getFqName() const {
-    return mFqName;
-}
-
 bool FqInstance::isValid() const {
     bool hasPkg = hasPackage();
     bool hasVer = hasVersion();
