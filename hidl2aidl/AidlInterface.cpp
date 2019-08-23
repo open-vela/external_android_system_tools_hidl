abd7dbf774b966794310fe77a3f3b45f24d048da
liujinye <liujinye@xiaomi.com>
2019-08-23 09:55:07 +0800

    Merge "Change (un)setLinePrefix to push and pop" am: 944f52c48c am: 4994befb3a
    Change-Id: I62d815cc594da8090861111c05702d2bced71d36

--- hidl2aidl/AidlInterface.cpp
+++ hidl2aidl/AidlInterface.cpp
@@ -16,7 +16,6 @@
 
 #include <android-base/logging.h>
 #include <android-base/parseint.h>
-#include <android-base/strings.h>
 #include <hidl-util/FQName.h>
 #include <hidl-util/Formatter.h>
 #include <hidl-util/StringHelper.h>
@@ -25,10 +24,7 @@
 
 #include "AidlHelper.h"
 #include "Coordinator.h"
-#include "DocComment.h"
-#include "FormattingConstants.h"
 #include "Interface.h"
-#include "Location.h"
 #include "Method.h"
 #include "NamedType.h"
 #include "Reference.h"
@@ -36,29 +32,11 @@
 
 namespace android {
 
-static void emitAidlMethodParams(WrappedOutput* wrappedOutput,
-                                 const std::vector<NamedReference<Type>*> args,
-                                 const std::string& prefix, const std::string& attachToLast,
-                                 const Interface& iface) {
-    if (args.size() == 0) {
-        *wrappedOutput << attachToLast;
-        return;
-    }
-
-    for (size_t i = 0; i < args.size(); i++) {
-        const NamedReference<Type>* arg = args[i];
-        std::string out =
-                prefix + AidlHelper::getAidlType(*arg->get(), iface.fqName()) + " " + arg->name();
-        wrappedOutput->group([&] {
-            if (i != 0) wrappedOutput->printUnlessWrapped(" ");
-            *wrappedOutput << out;
-            if (i == args.size() - 1) {
-                if (!attachToLast.empty()) *wrappedOutput << attachToLast;
-            } else {
-                *wrappedOutput << ",";
-            }
-        });
-    }
+static void emitAidlMethodParams(Formatter& out, const std::vector<NamedReference<Type>*> args,
+                                 const std::string& prefix, const Interface& iface) {
+    out.join(args.begin(), args.end(), ", ", [&](const NamedReference<Type>* arg) {
+        out << prefix << AidlHelper::getAidlType(*arg->get(), iface.fqName()) << " " << arg->name();
+    });
 }
 
 std::vector<const Method*> AidlHelper::getUserDefinedMethods(const Interface& interface) {
@@ -118,16 +96,6 @@ static void pushVersionedMethodOntoMap(MethodWithVersion versionedMethod,
     }
 }
 
-struct ResultTransformation {
-    enum class TransformType {
-        MOVED,    // Moved to the front of the method name
-        REMOVED,  // Removed the result
-    };
-
-    const std::string& resultName;
-    const TransformType& type;
-};
-
 void AidlHelper::emitAidl(const Interface& interface, const Coordinator& coordinator) {
     for (const NamedType* type : interface.getSubTypes()) {
         emitAidl(*type, coordinator);
@@ -167,16 +135,14 @@ void AidlHelper::emitAidl(const Interface& interface, const Coordinator& coordin
 
         out.join(methodMap.begin(), methodMap.end(), "\n", [&](const auto& pair) {
             const Method* method = pair.second.method;
+            method->emitDocComment(out);
 
             std::vector<NamedReference<Type>*> results;
-            std::vector<ResultTransformation> transformations;
             for (NamedReference<Type>* res : method->results()) {
                 if (StringHelper::EndsWith(StringHelper::Uppercase(res->name()), "STATUS") ||
                     StringHelper::EndsWith(StringHelper::Uppercase(res->name()), "ERROR")) {
                     out << "// Ignoring result " << getAidlType(*res->get(), interface.fqName())
                         << " " << res->name() << " since AIDL has built in status types.\n";
-                    transformations.emplace_back(ResultTransformation{
-                            res->name(), ResultTransformation::TransformType::REMOVED});
                 } else {
                     results.push_back(res);
                 }
@@ -193,69 +159,21 @@ void AidlHelper::emitAidl(const Interface& interface, const Coordinator& coordin
 
                 out << "// Adding return type to method instead of out param " << returnType << " "
                     << results[0]->name() << " since there is only one return value.\n";
-                transformations.emplace_back(ResultTransformation{
-                        results[0]->name(), ResultTransformation::TransformType::MOVED});
                 results.clear();
             }
 
-            if (method->getDocComment() != nullptr) {
-                std::vector<std::string> modifiedDocComment;
-                for (const std::string& line : method->getDocComment()->lines()) {
-                    std::vector<std::string> tokens = base::Split(line, " ");
-                    if (tokens.size() <= 1 || tokens[0] != "@return") {
-                        // unimportant line
-                        modifiedDocComment.emplace_back(line);
-                        continue;
-                    }
+            if (method->isOneway()) out << "oneway ";
+            out << returnType << " " << pair.second.name << "(";
+            emitAidlMethodParams(out, method->args(), "in ", interface);
 
-                    const std::string& res = tokens[1];
-                    bool transformed = false;
-                    for (const ResultTransformation& transform : transformations) {
-                        if (transform.resultName != res) continue;
-
-                        // Some transform was done to it
-                        if (transform.type == ResultTransformation::TransformType::MOVED) {
-                            // remove the name
-                            tokens.erase(++tokens.begin());
-                            transformed = true;
-                        } else {
-                            CHECK(transform.type == ResultTransformation::TransformType::REMOVED);
-                            tokens.insert(tokens.begin(), "The following return was removed\n");
-                            transformed = true;
-                        }
-                    }
-
-                    if (!transformed) {
-                        tokens.erase(tokens.begin());
-                        tokens.insert(tokens.begin(), "@param out");
-                    }
-
-                    modifiedDocComment.emplace_back(base::Join(tokens, " "));
-                }
-
-                DocComment(base::Join(modifiedDocComment, "\n"), HIDL_LOCATION_HERE).emit(out);
-            }
-
-            WrappedOutput wrappedOutput(MAX_LINE_LENGTH);
-
-            if (method->isOneway()) wrappedOutput << "oneway ";
-            wrappedOutput << returnType << " " << pair.second.name << "(";
-
-            if (results.empty()) {
-                emitAidlMethodParams(&wrappedOutput, method->args(), /* prefix */ "in ",
-                                     /* attachToLast */ ");\n", interface);
-            } else {
-                emitAidlMethodParams(&wrappedOutput, method->args(), /* prefix */ "in ",
-                                     /* attachToLast */ ",", interface);
-                wrappedOutput.printUnlessWrapped(" ");
-
+            // Join these
+            if (!results.empty()) {
                 // TODO: Emit warning if a primitive is given as a out param.
                 if (!method->args().empty()) out << ", ";
-                emitAidlMethodParams(&wrappedOutput, results, /* prefix */ "out ",
-                                     /* attachToLast */ ");\n", interface);
+                emitAidlMethodParams(out, results, "out ", interface);
             }
 
-            out << wrappedOutput;
+            out << ");\n";
         });
     });
 }
