370191c7b36082b6cdcc5f08810835eb7e845bd1
liujinye <liujinye@xiaomi.com>
2019-08-23 10:24:52 +0800

    Merge "Change (un)setLinePrefix to push and pop" am: 944f52c48c am: 4994befb3a am: 26ea65e7b5 am: 363d8e86d9
    Change-Id: I12c0fc689020cf6dc322dcc255e1ab95ad815371

--- hidl2aidl/AidlInterface.cpp
+++ hidl2aidl/AidlInterface.cpp
@@ -24,7 +24,6 @@
 
 #include "AidlHelper.h"
 #include "Coordinator.h"
-#include "FormattingConstants.h"
 #include "Interface.h"
 #include "Method.h"
 #include "NamedType.h"
@@ -33,29 +32,11 @@
 
 namespace android {
 
-static void emitAidlMethodParams(WrappedOutput* wrappedOutput,
-                                 const std::vector<NamedReference<Type>*> args,
-                                 const std::string& prefix, const std::string& attachToLast,
-                                 const Interface& iface) {
-    if (args.size() == 0) {
-        *wrappedOutput << attachToLast;
-        return;
-    }
-
-    for (size_t i = 0; i < args.size(); i++) {
-        const NamedReference<Type>* arg = args[i];
-        std::string out =
-                prefix + AidlHelper::getAidlType(*arg->get(), iface.fqName()) + " " + arg->name();
-        wrappedOutput->group([&] {
-            if (i != 0) wrappedOutput->printUnlessWrapped(" ");
-            *wrappedOutput << out;
-            if (i == args.size() - 1) {
-                if (!attachToLast.empty()) *wrappedOutput << attachToLast;
-            } else {
-                *wrappedOutput << ",";
-            }
-        });
-    }
+static void emitAidlMethodParams(Formatter& out, const std::vector<NamedReference<Type>*> args,
+                                 const std::string& prefix, const Interface& iface) {
+    out.join(args.begin(), args.end(), ", ", [&](const NamedReference<Type>* arg) {
+        out << prefix << AidlHelper::getAidlType(*arg->get(), iface.fqName()) << " " << arg->name();
+    });
 }
 
 std::vector<const Method*> AidlHelper::getUserDefinedMethods(const Interface& interface) {
@@ -181,26 +162,18 @@ void AidlHelper::emitAidl(const Interface& interface, const Coordinator& coordin
                 results.clear();
             }
 
-            WrappedOutput wrappedOutput(MAX_LINE_LENGTH);
+            if (method->isOneway()) out << "oneway ";
+            out << returnType << " " << pair.second.name << "(";
+            emitAidlMethodParams(out, method->args(), "in ", interface);
 
-            if (method->isOneway()) wrappedOutput << "oneway ";
-            wrappedOutput << returnType << " " << pair.second.name << "(";
-
-            if (results.empty()) {
-                emitAidlMethodParams(&wrappedOutput, method->args(), /* prefix */ "in ",
-                                     /* attachToLast */ ");\n", interface);
-            } else {
-                emitAidlMethodParams(&wrappedOutput, method->args(), /* prefix */ "in ",
-                                     /* attachToLast */ ",", interface);
-                wrappedOutput.printUnlessWrapped(" ");
-
+            // Join these
+            if (!results.empty()) {
                 // TODO: Emit warning if a primitive is given as a out param.
                 if (!method->args().empty()) out << ", ";
-                emitAidlMethodParams(&wrappedOutput, results, /* prefix */ "out ",
-                                     /* attachToLast */ ");\n", interface);
+                emitAidlMethodParams(out, results, "out ", interface);
             }
 
-            out << wrappedOutput;
+            out << ");\n";
         });
     });
 }
