c497b7ff6a03861330d7b74acce610bf8dcda23a
liujinye <liujinye@xiaomi.com>
2020-03-10 16:53:50 +0800

    Add explicit builds for system_ext/product variants. am: 643fe77523 am: 2905f84ac5
    Change-Id: Ifa362c1f6d3b443c07b84854d5097eb38bb768e8

--- build/hidl_interface.go
+++ build/hidl_interface.go
@@ -464,10 +464,6 @@ type hidlInterfaceProperties struct {
 
 	// example: -randroid.hardware:hardware/interfaces
 	Full_root_option string `blueprint:"mutated"`
-
-	// Whether this interface library should be installed on product partition.
-	// TODO(b/150902910): remove, since this should be an inherited property.
-	Product_specific *bool
 }
 
 type hidlInterface struct {
@@ -577,9 +573,6 @@ This corresponds to the "-r%s:<some path>" option that would be passed into hidl
 	shouldGenerateJava := proptools.BoolDefault(i.properties.Gen_java, true)
 	shouldGenerateJavaConstants := i.properties.Gen_java_constants
 	shouldGenerateVts := shouldGenerateLibrary && proptools.BoolDefault(i.properties.Gen_vts, true)
-
-	// TODO(b/150902910): re-enable VTS builds for product things
-	shouldGenerateVts = shouldGenerateVts && !proptools.Bool(i.properties.Product_specific)
 
 	var libraryIfExists []string
 	if shouldGenerateLibrary {
@@ -927,8 +920,6 @@ var doubleLoadablePackageNames = []string{
 	"android.hardware.graphics.allocator@",
 	"android.hardware.graphics.bufferqueue@",
 	"android.hardware.media@",
-	"android.hardware.media.bufferpool@",
-	"android.hardware.media.c2@",
 	"android.hardware.media.omx@",
 	"android.hardware.memtrack@1.0",
 	"android.hardware.neuralnetworks@",
