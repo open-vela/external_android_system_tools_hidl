408a59baf12d6dbd2786644b76dc72462c0cb42f
liujinye <liujinye@xiaomi.com>
2020-07-06 21:53:05 +0800

    -Landroidbp: keep the list of libs to put in VNDK am: d421ec1c66
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/tools/hidl/+/12064885
    
    Change-Id: I4a1b42342c1061a5919513e0500a8cbd8e4fd918

--- test/run_all_device_tests.sh
+++ test/run_all_device_tests.sh
@@ -2,7 +2,6 @@
 
 function run() {
     local FAILED_TESTS=()
-    local SKIPPED_TESTS=()
 
     # Tests directly relevant to HIDL infrustructure but aren't
     # located in system/tools/hidl
@@ -34,24 +33,11 @@ function run() {
     local BITNESS=("nativetest" "nativetest64")
 
     for test in ${RUN_TIME_TESTS[@]}; do
-        local ran_test=0
         for bits in ${BITNESS[@]}; do
-            local test_binary="/data/$bits/$test/$test"
-
-            # only run test if binary is installed
-            if adb shell sh -c '"[ -f '$test_binary' ]"'; then
-                ran_test=1
-            else
-                SKIPPED_TESTS+=("$bits:$test")
-                break
-            fi
-
-            echo $test_binary
-            adb shell $test_binary || FAILED_TESTS+=("$bits:$test")
+            echo $bits $test
+            adb shell /data/$bits/$test/$test ||
+                FAILED_TESTS+=("$bits:$test")
         done
-        if [ $ran_test -ne 1 ]; then
-            FAILED_TESTS+=("$test (not installed)")
-        fi
     done
 
     for test in ${SCRIPT_TESTS[@]}; do
@@ -63,11 +49,6 @@ function run() {
     echo
     echo ===== ALL DEVICE TESTS SUMMARY =====
     echo
-    if [ ${#SKIPPED_TESTS[@]} -gt 0 ]; then
-        for skipped in ${SKIPPED_TEST[@]}; do
-            echo "SKIPPED TEST: $skipped"
-        done
-    fi
     if [ ${#FAILED_TESTS[@]} -gt 0 ]; then
         for failed in ${FAILED_TESTS[@]}; do
             echo "FAILED TEST: $failed"
