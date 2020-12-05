09efb10116042ab495974ce4aa6819f39fb81576
Steven Moreland <smoreland@google.com>
2020-12-05 02:02:46 +0000

    hidl-gen: warnings for bad annotations
    hidl-gen never paid attention to annotations, and we can see in-tree
    that there are some annotatoins which don't do anything.
    
    Warnings instead of errors, since it doesn't make sense to break
    downstream (might enable them after a long time if it became more
    important).
    
    Bug: 173732508
    Test: build/test with adding errors to cleanup AOSP interfaces
    Change-Id: I5b68a25f5e39239b2061c0e5186710f5cbf12c22

--- Interface.cpp
+++ Interface.cpp
@@ -537,9 +537,6 @@ status_t Interface::validate() const {
     err = validateUniqueNames();
     if (err != OK) return err;
 
-    err = validateAnnotations();
-    if (err != OK) return err;
-
     return Scope::validate();
 }
 
@@ -583,6 +580,19 @@ status_t Interface::validateUniqueNames() const {
 }
 
 status_t Interface::validateAnnotations() const {
+    for (const Annotation* annotation : annotations()) {
+        const std::string name = annotation->name();
+
+        if (name == "SensitiveData") {
+            continue;
+        }
+
+        std::cerr << "WARNING: Unrecognized annotation '" << name << "' for " << typeName()
+                  << " at " << location() << ". Only @SensitiveData is supported." << std::endl;
+        // ideally would be error, but we don't want to break downstream
+        // return UNKNOWN_ERROR;
+    }
+
     for (const Method* method : methods()) {
         for (const Annotation* annotation : method->annotations()) {
             const std::string name = annotation->name();
@@ -592,12 +602,13 @@ status_t Interface::validateAnnotations() const {
             }
 
             std::cerr << "ERROR: Unrecognized annotation '" << name
-                      << "' for method: " << method->name() << ". An annotation should be one of: "
-                      << "entry, exit, callflow." << std::endl;
+                      << "' for method: " << method->name() << " at " << method->location()
+                      << ". An annotation should be one of: "
+                      << "@entry, @exit, or @callflow." << std::endl;
             return UNKNOWN_ERROR;
         }
     }
-    return OK;
+    return OK;  // not calling superclass which is more restrictive
 }
 
 bool Interface::addAllReservedMethods() {
