223c56ac1b37bfb39efed32bb1035e690e4de8a7
openvela-robot <openvela-robot@xiaomi.com>
2020-12-09 17:15:02 +0800

    Merge "hidl2aidl: generate numeric_limits call instead of value" am: d9dc03ebb8 am: 275f2de136
    Original change: https://android-review.googlesource.com/c/platform/system/tools/hidl/+/1520540
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I73beb6bab89eba521ff21de48c751c6feb6b234b

--- hidl2aidl/AidlType.cpp
+++ hidl2aidl/AidlType.cpp
@@ -19,7 +19,6 @@
 
 #include "AidlHelper.h"
 #include "ArrayType.h"
-#include "EnumType.h"
 #include "FmqType.h"
 #include "NamedType.h"
 #include "Type.h"
@@ -84,9 +83,6 @@ std::string AidlHelper::getAidlType(const Type& type, const FQName& relativeTo) 
         // enum type goes to the primitive java type in HIDL, but AIDL should use
         // the enum type name itself
         return type.definedName();
-    } else if (type.isBitField()) {
-        const BitFieldType& bitfield = static_cast<const BitFieldType&>(type);
-        return getAidlType(*bitfield.getElementType(), relativeTo);
     } else {
         return type.getJavaType();
     }
